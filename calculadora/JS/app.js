2d33b9f72b5b087d701174f52d9c37b4b79b4dd5
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-03-10 16:36:40 -0600

    Update calculadora
    Se agregaron las operaciones basicas para la calculadora

--- calculadora/JS/app.js
+++ calculadora/JS/app.js
@@ -1,5 +1,14 @@
-const digitNumber = (digit) => {
-    const display = document.querySelector('.display');
-    display.value += digit;
+function digitNumber(digit){
+    document.getElementById('display').value += digit;
 }
 
+
+function clearDis(){
+    document.getElementById('display').value = '';
+}
+
+function result() {
+    var display = document.getElementById('display').value;
+    var result = eval(display);
+    document.getElementById('display').value = result;
+  }
