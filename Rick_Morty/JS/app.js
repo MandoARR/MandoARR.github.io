15f6a755bd7358d0fff85919a0f99b30a36f979d
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-03-19 15:36:36 -0600

    API_Rick&Morty
    Creacion de la API Rick & Morty

---
+++ Rick_Morty/JS/app.js
@@ -0,0 +1,26 @@
+const url = "https://rickandmortyapi.com/api/character"
+
+function api_rick_morty() {
+    fetch(url).then(res => res.json())
+        .then(data => {
+            //console.log(data)
+            personajes(data)
+        })
+}
+
+function personajes(json) {
+    const personajes = json.results
+
+    personajes.forEach(personaje => {
+        const row = document.createElement("tr")
+
+        row.innerHTML = `<td>${personaje.name}</td>
+                        <td>${personaje.status}</td>
+                        <td>${personaje.species}</td>
+                        <td><img src="${personaje.image}"></img></td>
+                        `
+        document.getElementById("listRick_Morty").appendChild(row)
+    });
+}
+
+api_rick_morty()
