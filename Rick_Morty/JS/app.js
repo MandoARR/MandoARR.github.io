796b4760b3b3144029d0e8040eb89a58c10fd93a
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-03-19 16:24:41 -0600

    API_Rick_Morty v0.1

--- Rick_Morty/JS/app.js
+++ Rick_Morty/JS/app.js
@@ -1,4 +1,5 @@
 const url = "https://rickandmortyapi.com/api/character"
+const cardTemplate = document.getElementById("scTemplate").innerHTML
 
 function api_rick_morty() {
     fetch(url).then(res => res.json())
@@ -10,17 +11,17 @@ function api_rick_morty() {
 
 function personajes(json) {
     const personajes = json.results
+    document.getElementById("content").innerHTML = ""
 
     personajes.forEach(personaje => {
-        const row = document.createElement("tr")
-        //const origin = personaje.origin
-        row.innerHTML = `<td>${personaje.name}</td>
-                        <td>${personaje.status}</td>
-                        <td>${personaje.species}</td>
-                        <td><img src="${personaje.image}"></img></td>
-                        <td>${personaje.origin.name}</td>
-                        `
-        document.getElementById("listRick_Morty").appendChild(row)
+        const origin = personaje.origin
+
+        document.getElementById("content").innerHTML += cardTemplate
+        .replace("_NAME_PJ_", personaje.name)
+        .replace("_URL_IMAGE_", personaje.image)
+        .replace("_NAME_SPECIE_", personaje.species)
+        .replace("_STATUS_", personaje.status)
+        .replace("_ORIGIN_", origin.name)
     });
 }
 
