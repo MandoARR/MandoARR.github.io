47947b8f984ebb61802f7e5c4e2a013906542925
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-06-12 16:45:33 -0600

    Update

--- list_add/src/components/List_add.tsx
+++ list_add/src/components/List_add.tsx
@@ -1,14 +1,25 @@
-import { useState } from "react"
+import { useState, FormEvent } from "react"
 
 export function List_add() {
     const [list, setList] = useState<[]>([])
 
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        //const bodyData = 
+    }
+
     return (
         <section className="secForm">
-            <form>
-                <input></input>
-                <button>ADD</button>
-            </form>
+            <div className="secEncabezado">
+                <h1>Hola</h1>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="textAdd"></input>
+                    <button type="submit">ADD</button>
+                </form>
+            </div>
         </section>
     )
 }
