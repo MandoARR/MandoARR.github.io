c76ee4ac61d26b32acf30950f07a2c65912fb38c
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-06-12 22:20:52 -0600

    update

--- list_add/src/components/List_add.tsx
+++ list_add/src/components/List_add.tsx
@@ -1,12 +1,23 @@
 import { useState, FormEvent } from "react"
 
-export function List_add() {
-    const [list, setList] = useState<[]>([])
+interface IlistAdd{
+    something: string
+}
 
-    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+export function List_add() {
+    const [list, setList] = useState<IlistAdd[]>([])
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        //const bodyData = 
+        const formData = new FormData(e.currentTarget)
+        const textAdd = formData.get("textAdd") as string
+
+        setList([...list, {something:textAdd}])
+    }
+
+    const handleDelete = (data) => {
+        data = null
     }
 
     return (
@@ -19,6 +30,18 @@ export function List_add() {
                     <input type="text" name="textAdd"></input>
                     <button type="submit">ADD</button>
                 </form>
+            </div>
+            <div>
+                <ul>
+                    {list.map((element) => (
+                        <>
+                            <li>
+                                {element.something}
+                                <button onClick={() => handleDelete(element.something)}>X</button>
+                            </li>
+                        </>
+                    ))}
+                </ul>
             </div>
         </section>
     )
